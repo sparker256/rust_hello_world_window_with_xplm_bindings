fdf6934c51266df5e743fa63d335a1f2f1b26cb0
sparker256 <bgood@velocity.net>
2023-01-10 16:17:43 -0500

    Working on Rust Wello World Window

--- src/plugin.rs
+++ src/plugin.rs
@@ -2,7 +2,9 @@
 #![allow(unused_imports)]
 #[allow(dead_code)]
 use crate::bindings;
-use bindings::{XPLMDrawString, XPLMFontID, XPLMSetGraphicsState, xplmFont_Proportional};
+use bindings::{XPLMDrawString, XPLMFontID, XPLMSetGraphicsState, xplmFont_Proportional, XPLMDebugString,
+    XPLMWindowID, XPLMCreateWindow_t, XPLMKeyFlags, xplm_WindowLayerFloatingWindows,
+    xplm_WindowDecorationRoundRectangle, XPLMCreateWindowEx, xplm_CursorDefault};
 
 
 use enum_primitive_derive::Primitive;
@@ -10,6 +12,45 @@ use enum_primitive_derive::Primitive;
 use std::ffi::{c_char, c_int, c_void, CString};
 use std::ptr;
 use std::ptr::null_mut;
+
+unsafe extern "C" fn dummy_mouse_handler(in_window_id: XPLMWindowID,
+    x: ::std::os::raw::c_int,
+    y: ::std::os::raw::c_int,
+    is_down: ::std::os::raw::c_int,
+    in_refcon: *mut ::std::os::raw::c_void) -> ::std::os::raw::c_int
+{
+    return 0;
+}
+
+unsafe extern "C" fn dummy_wheel_handler(in_window_id: XPLMWindowID,
+    x: ::std::os::raw::c_int,
+    y: ::std::os::raw::c_int,
+    wheel: ::std::os::raw::c_int,
+    clicks: ::std::os::raw::c_int,
+    in_refcon: *mut ::std::os::raw::c_void) -> ::std::os::raw::c_int
+{
+     return 0;
+}
+
+unsafe extern "C" fn dummy_key_handler( in_window_id: XPLMWindowID,
+    key: i8,
+    flags: XPLMKeyFlags,
+    virtual_key: i8,
+    in_refcon: *mut ::std::os::raw::c_void ,
+    losing_focus: ::std::os::raw::c_int )
+{
+
+}
+
+unsafe extern "C" fn dummy_cursor_status_handler(in_window_id: XPLMWindowID,
+    x: ::std::os::raw::c_int,
+    iy: ::std::os::raw::c_int,
+    in_refcon: *mut ::std::os::raw::c_void) -> i32
+{
+    return xplm_CursorDefault.try_into().unwrap();
+}
+
+
 
 unsafe fn copy_to_c_buffer(src: String, dest: *mut c_char) {
     let src_c = CString::new(src).unwrap_or_else(|_| CString::new("<invalid>").unwrap());
@@ -26,6 +67,34 @@ unsafe extern "C" fn XPluginStart(
     copy_to_c_buffer(String::from("Rust Hello World"), name);
     copy_to_c_buffer(String::from("org.SparkerInVR.rust.hello.world"), signature);
     copy_to_c_buffer(String::from("Rust Hello World Plugin"), description);
+
+    let mut window_params = XPLMCreateWindow_t {
+        structSize: std::mem::size_of::<XPLMCreateWindow_t>() as i32,
+        left: 0,
+        top: 800,
+        right: 500,
+        bottom: 500,
+        visible: true as i32,
+
+        drawWindowFunc: Some(draw_hello_world),
+        handleMouseClickFunc: Some(dummy_mouse_handler),
+        handleRightClickFunc: Some(dummy_mouse_handler),
+        handleMouseWheelFunc: Some(dummy_wheel_handler),
+        handleKeyFunc: Some(dummy_key_handler),
+        handleCursorFunc: Some(dummy_cursor_status_handler),
+        refcon: null_mut(),
+        decorateAsFloatingWindow: xplm_WindowDecorationRoundRectangle as i32,
+        layer: xplm_WindowLayerFloatingWindows as i32,
+    };
+    
+    // Need to find the issue with the next line as window_data.window_id is not working
+    // Need to find something to replace it with in my example
+    
+    // window_data.window_id = unsafe { XPLMCreateWindowEx(&mut window_params) };
+
+
+    
+
     1
 }
 
@@ -34,7 +103,7 @@ unsafe extern "C" fn XPluginEnable() -> c_int {
     println!("[hello-plugin-rust] Enabled");
     const BUF_NAME: &str = "Rust Hello World Enabled!\n";
     let name = CString::new(BUF_NAME).expect("");
-    bindings::XPLMDebugString(name.as_ptr());
+    XPLMDebugString(name.as_ptr());
     1
 }
 
@@ -43,7 +112,7 @@ unsafe extern "C" fn XPluginDisable() {
     println!("[hello-plugin-rust] Disabled");
     const BUF_NAME: &str = "Rust Hello World Disabled!\n";
     let name = CString::new(BUF_NAME).expect("");
-    bindings::XPLMDebugString(name.as_ptr());
+    XPLMDebugString(name.as_ptr());
 }
 
 #[no_mangle]
@@ -61,7 +130,7 @@ unsafe extern "C" fn XPluginReceiveMessage(
 
 // This now seems to match my Hello-World-SDK-4 draw_hello_world function
 unsafe extern "C" fn draw_hello_world(
-    hd_window_id: bindings::XPLMWindowID,
+    hd_window_id: XPLMWindowID,
     hw_refcon: *mut ::std::os::raw::c_void,
 ) {
     // Mandatory: We *must* set the OpenGL state before drawing
