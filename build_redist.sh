0d3e9e84d36cb6ee2e21ad1b2edb2072f154c060
sparker256 <bgood@velocity.net>
2023-01-13 22:12:03 -0500

    Update Build process

--- build_redist.sh
+++ build_redist.sh
@@ -10,12 +10,10 @@ cargo build --release --target=x86_64-apple-darwin
 cargo build --release --target=x86_64-pc-windows-gnu
 cargo build --release --target=x86_64-unknown-linux-gnu
 
-mkdir -p "${PLUGIN_TARGET}/lin_x64" "${PLUGIN_TARGET}/mac_x64" "${PLUGIN_TARGET}/win_x64"
+lipo -create -output "${PROJECT}/mac_x64/${PROJECT}.xpl" "target/x86_64-apple-darwin/release/lib${PROJECT}.dylib" "target/aarch64-apple-darwin/release/lib${PROJECT}.dylib"
 
-lipo -create -output "target/dist/${PROJECT}/mac_x64/${PROJECT}.xpl" "target/x86_64-apple-darwin/release/lib${PROJECT}.dylib" "target/aarch64-apple-darwin/release/lib${PROJECT}.dylib"
-
-cp -v "target/x86_64-unknown-linux-gnu/release/lib${PROJECT}.so" "target/dist/${PROJECT}/lin_x64/${PROJECT}.xpl"
-cp -v "target/x86_64-pc-windows-gnu/release/${PROJECT}.dll" "target/dist/${PROJECT}/win_x64/${PROJECT}.xpl"
+cp -v "target/x86_64-unknown-linux-gnu/release/lib${PROJECT}.so" "${PROJECT}/lin_x64/${PROJECT}.xpl"
+cp -v "target/x86_64-pc-windows-gnu/release/${PROJECT}.dll" "${PROJECT}/win_x64/${PROJECT}.xpl"
 
 echo "Distribution built at ${PLUGIN_TARGET}"
 
